dd51698f1a51129b35d6e3252b50bb8aa245b4cd
sergeyvip84 <87651037+sergeyvip84@users.noreply.github.com>
2022-05-30 08:10:58 +0300

    Initial Commit

---
+++ FotoApi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FotoApi
+//
+//  Created by Serhii Palamarchuk on 30.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
